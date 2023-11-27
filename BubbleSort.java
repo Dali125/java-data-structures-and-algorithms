af9436f5f9b3333ad056c3c1c940840e3c81c488
Dali125 <82496809+Dali125@users.noreply.github.com>
2023-11-27 23:21:32 +0200

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,48 @@
+public class BubbleSort(){
+
+
+
+  public int[] sort(int arr[]){
+
+
+    int length = arr.length();
+
+    for(int i = 0; k < length -1; i++){
+
+      for(int k = 0; i < length - i - 1){
+
+        if(arr[i] > arr[i+1]{
+
+          int temp = arr[i];
+          arr[i] = arr[i -1];
+          arr[i-1] = temp;
+
+
+
+
+        }
+
+
+
+
+
+
+      }
+
+
+
+
+    }
+
+
+  }
+
+
+
+
+
+
+
+
+
+}
