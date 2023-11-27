31723105ba85690dc320e44151a281c518033605
Dali125 <82496809+Dali125@users.noreply.github.com>
2023-11-27 23:23:07 +0200

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,48 +1,31 @@
-public class BubbleSort(){
+public class BubbleSort {
 
+  public int[] sort(int arr[]) {
+    int length = arr.length;
 
-
-  public int[] sort(int arr[]){
-
-
-    int length = arr.length();
-
-    for(int i = 0; k < length -1; i++){
-
-      for(int k = 0; i < length - i - 1){
-
-        if(arr[i] > arr[i+1]{
-
-          int temp = arr[i];
-          arr[i] = arr[i -1];
-          arr[i-1] = temp;
-
-
-
-
+    for (int i = 0; i < length - 1; i++) {
+      for (int k = 0; k < length - i - 1; k++) {
+        if (arr[k] > arr[k + 1]) {
+          // Swap arr[k] and arr[k+1]
+          int temp = arr[k];
+          arr[k] = arr[k + 1];
+          arr[k + 1] = temp;
         }
-
-
-
-
-
-
       }
-
-
-
-
     }
 
-
+    return arr;
   }
 
+  public static void main(String[] args) {
+    // Example usage
+    int[] arrayToSort = {64, 34, 25, 12, 22, 11, 90};
+    BubbleSort bubbleSort = new BubbleSort();
+    int[] sortedArray = bubbleSort.sort(arrayToSort);
 
-
-
-
-
-
-
-
+    // Print the sorted array
+    for (int value : sortedArray) {
+      System.out.print(value + " ");
+    }
+  }
 }
